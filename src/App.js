53d354e7c8a5a3bc328a602468a7baeef10e442f
@Jayanthkumar_001 <2000030043@kluniversity.in>
2023-03-23 20:42:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Contact from "./routes/Contact";
 
 import { Route, Routes } from 'react-router-dom';
 function App() {
-  return (adaa
-    <>
+  return (
+    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       {/* <Route path="/projects" element={<Projects />} /> */}
     </Routes>
-    </>
+    
   );
 }
 
